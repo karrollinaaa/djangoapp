951654403f864aa25ca72b9bfcf70b770ca20176
karrollinaaa <karolinab503@gmail.com>
2019-12-05 08:04:29 +0100

    pawel

--- users/views.py
+++ users/views.py
@@ -3,9 +3,10 @@ from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from users.forms import UserLoginForm
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
-    pass
+    return render(request, 'users/index.html')
 
 def rejestruj(request):
     if request.method == 'POST':
@@ -21,10 +22,25 @@ def rejestruj(request):
 
 def loguj_user(request):
     if request.method == 'POST':
-        pass
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            nazwa = form.cleaned_data['login']
+            haslo = form.cleaned_data['haslo']
+            user = authenticate(request, username=nazwa, password=haslo)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Zostałeś zalogowany!")
+                return redirect(reverse('users:index'))
+            else:
+                messages.error(request, "Błędny login lub hasło!")
     else:
         form = UserLoginForm()
 
     kontekst = {'form': form}
     return render(request, 'users/loguj_user.html', kontekst)
+
+def wyloguj_user(request):
+    logout(request)
+    messages.info(request, "Zostałeś wylogowany!")
+    return redirect(reverse('users:index'))
 
